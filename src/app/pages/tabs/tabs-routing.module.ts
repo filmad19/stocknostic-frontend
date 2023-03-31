8014559c6abea7ef092f1e9ecb7ae3c6b52f3ce2
filmad19 <filmad19@htl-kaindorf.at>
2023-03-31 12:12:46 +0200

    frontend structure

---
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,70 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+// const routes: Routes = [
+//   {
+//     path: 'tabs',
+//     component: TabsPage,
+//     children: [
+//       {
+//         path: 'tab1',
+//         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+//       },
+//       {
+//         path: 'tab2',
+//         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+//       },
+//       {
+//         path: 'tab3',
+//         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+//       },
+//       {
+//         path: '',
+//         redirectTo: '/tabs/tab1',
+//         pathMatch: 'full'
+//       }
+//     ]
+//   },
+//   {
+//     path: '',
+//     redirectTo: '/tabs/tab1',
+//     pathMatch: 'full'
+//   }
+// ];
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'search',
+        loadChildren: () => import('../search/search.module').then(m => m.SearchPageModule)
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'settings',
+        loadChildren: () => import('../settings/setting.module').then(m => m.SettingPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+})
+export class TabsPageRoutingModule {}
